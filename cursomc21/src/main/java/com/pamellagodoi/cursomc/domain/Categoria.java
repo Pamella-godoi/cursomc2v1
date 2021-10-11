00d517d7789fc0dc306e5e57660b0e3c06b979ed
Pamella Godoi <pamella.godoi@gmail.com>
2021-10-11 20:35:43 -0300

    trabalhando com banco H2. foi criado nova versao de pasta

---
+++ cursomc21/src/main/java/com/pamellagodoi/cursomc/domain/Categoria.java
@@ -0,0 +1,76 @@
+package com.pamellagodoi.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+//Serializable -> informa que os arquivos podem ser trafegados 
+public class Categoria implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	
+	
+	// construtor
+	public Categoria() {
+		
+	}
+
+	//construtor com atributos
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	// getts and setts 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	//hash code and equals -> consistencia de valores recebido na variavel principal
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+
+}
